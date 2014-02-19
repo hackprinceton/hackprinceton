66928b4fcd23d09ab66853a18569eaa22ca96f4f
Hansen Qian <Hansenq@gmail.com>
2014-02-19 12:15:54 -0500

    Added randomizing images, renamed img/ files, set maximum size of photo

--- new/js/script.js
+++ new/js/script.js
@@ -27,3 +27,19 @@ $(".sponsor").click(function () {
         scrollTop: $('#sponsor').offset().top
     }, 'slow');
 });
+
+$(document).ready(function () {
+    $('.header').css({
+        'position': 'relative',
+        'background': 'url(img/bg' + Math.floor(Math.random() * 8) + '.jpg) no-repeat center center fixed',
+        'background-position': 'center',
+        'background-size': 'cover',
+        '-webkit-background-size': 'cover',
+        '-moz-background-size': 'cover',
+        '-o-background-size': 'cover',
+        'text-align': 'center',
+        'height': '100vh',
+        'min-height': '600px',
+        'max-height': '1500px'
+    });
+});
