ee03bf5890c76782ee1e992bab651634498d23f4
Tony Jin <tzjin@princeton.edu>
2014-02-20 18:34:50 -0500

    Added signup page, with both signup forms

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-// Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-44449737-1', 'hackprinceton.com');
-ga('send', 'pageview');
-
-
 $(".signup").click(function () {
     $('html, body').animate({
         scrollTop: $('#signup').offset().top
@@ -52,4 +42,19 @@ $(document).ready(function () {
         'min-height': '600px',
         'max-height': '1500px'
     });
+  
+  $('#student').change(function () {
+    if ($(this).val() == "princeton") {
+      $('#prince').slideDown('fast');
+      $('#other').slideUp('fast');
+    } else if ($(this).val() == "school") {
+      $('#other').slideDown('fast');
+      $('#prince').slideUp('fast');
+    } 
+    else {
+      $('#other').slideUp('fast');
+      $('#prince').slideUp('fast');
+
+    }
+  });
 });
