04797be0e4a25c5e2540434c9f835bcc67aeb589
Collin Stedman <cstedman@princeton>
2014-07-18 09:43:00 -0700

    Created archive and began cleanup

---
+++ collin/js/script.js
@@ -0,0 +1,13 @@
+var body = $('body');
+$('#box1').click(function() {
+  body.css({ 'left': '-110%' });
+});
+$('#box2').click(function() {
+  body.css({ 'left': '-220%' });
+});
+$('#box3').click(function() {
+  body.css({ 'left': '-330%' });
+});
+$('#box4').click(function() {
+  body.css({ 'left': '0%' });
+});
