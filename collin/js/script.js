ad348787a7f7ad7d0fbe41f61c0c5c585ad0e018
Collin Stedman <cstedman@princeton>
2014-07-21 01:12:11 -0700

    Added scroll jacking

--- collin/js/script.js
+++ collin/js/script.js
@@ -1,19 +1,40 @@
 var body = $('body');
-$('#box1').click(function() {
-  body.css({ 'left': '-110%' });
-});
-$('#box2').click(function() {
-  body.css({ 'left': '-220%' });
-});
-$('#box3').click(function() {
-  body.css({ 'left': '0%' });
-});
-$('#box4').click(function() {
-  body.css({ 'left': '0%' });
-});
+
+var index = 0;
 
 $('#menu li').click(function() {
-  var index = $(this).index();
+  index = $(this).index();
   var offset = -(index * 110) + '%';
   body.css({ 'left': offset });
+});
+
+var lastPos = 0;
+var triggered = false;
+$(window).scroll(function() {
+  if (!triggered) {
+    triggered = true;
+    
+    var currPos = $(document).scrollLeft();
+    
+    if (lastPos < currPos && index < 3) {
+      index++;
+      var offset = -(index * 110) + '%';
+      body.css({ 'left': offset });
+      setTimeout(function() {
+        triggered = false;
+      }, 1000);
+    } 
+    else if (lastPos > currPos && index > 0) 
+    {
+      index--;
+      var offset = -(index * 110) + '%';
+      body.css({ 'left': offset });
+      setTimeout(function() {
+        triggered = false;
+      }, 1000);
+    }
+    else {
+      triggered = false;
+    }
+  }
 });
