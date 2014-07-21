cf4ff71d6ce0e4a1f0050dff397a00aeaf2f5bd6
Tony Jin <tzjin@princeton.edu>
2014-07-21 11:17:42 -0700

    Prelimary floating cards layout

--- collin/js/script.js
+++ collin/js/script.js
@@ -13,18 +13,18 @@ var triggered = false;
 $(window).scroll(function() {
   if (!triggered) {
     triggered = true;
-    
+
     var currPos = $(document).scrollLeft();
-    
-    if (lastPos < currPos && index < 3) {
+
+    if (lastPos < currPos && index < 4) {
       index++;
       var offset = -(index * 110) + '%';
       body.css({ 'left': offset });
       setTimeout(function() {
         triggered = false;
       }, 1000);
-    } 
-    else if (lastPos > currPos && index > 0) 
+    }
+    else if (lastPos > currPos && index > 0)
     {
       index--;
       var offset = -(index * 110) + '%';
