da95b4bb251bf93d6108fa0e3f06cce0c9839d66
Collin Stedman <cstedman@princeton>
2014-07-20 23:30:53 -0700

    Box logic sans styling and menu-based scroll

--- collin/js/script.js
+++ collin/js/script.js
@@ -6,8 +6,14 @@ $('#box2').click(function() {
   body.css({ 'left': '-220%' });
 });
 $('#box3').click(function() {
-  body.css({ 'left': '-330%' });
+  body.css({ 'left': '0%' });
 });
 $('#box4').click(function() {
   body.css({ 'left': '0%' });
+});
+
+$('#menu li').click(function() {
+  var index = $(this).index();
+  var offset = -(index * 110) + '%';
+  body.css({ 'left': offset });
 });
