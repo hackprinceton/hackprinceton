adf519fc449077e6e191c0a852fbdbe7b41b815e
Tony Jin <tzjin@princeton.edu>
2014-09-15 00:03:21 -0400

    Team changes in signin

--- js/signin.js
+++ js/signin.js
@@ -1,4 +1,17 @@
+function invalidTeam(message) {
+  team = null;
+  $("#team").css("border","red solid 1px");
+  $("#teammessage").text(message)
+}
+
+function validTeam() {
+  $("#team").css("border","green solid 1px");
+  $("#teammessage").text("Team successfully joined!")
+}
+
 $(document).ready(function(){
+  var TeamList = Parse.Object.extend("Teams");
+
   $("#signin").submit(function(event){
     event.preventDefault();
 
@@ -6,7 +19,6 @@ $(document).ready(function(){
     Parse.User.logIn($('#email').val(), $('#password').val(), {
       success: function(user) {
         // Do stuff after successful login.
-
         $('#form').hide();
         $('#status').text(user.get('status'));
         $('#code').text(user.get('team'));
@@ -14,6 +26,57 @@ $(document).ready(function(){
       },
       error: function(user, error) {
         // The login failed. Check error to see why.
+      }
+    });
+  }),
+
+  // Team number inputted
+  $('#team').change(function(event) {
+    var user = Parse.User.current();
+    var input = $('#team').val();
+
+    if (input === user.get("team")) {
+      invalidTeam("Already on team!");
+    }
+
+    var query = new Parse.Query(TeamList);
+    query.equalTo("teamcode", input);
+
+    query.first({
+      success: function(object) {
+        if (object == null) {
+          invalidTeam("Team not found.");
+        } else if (object.get("count") > 5) {
+          invalidTeam("Team already full!");
+        } else {
+
+          var teamQuery = new Parse.Query(TeamList);
+          teamQuery.equalTo("teamcode", user.get("team"));
+          teamQuery.first({
+              success: function(team) {
+                team.increment("count", -1);
+                team.save();
+              },
+              error: function(error) {
+                invalidTeam("Error: " + error.code + " " + error.message);
+              }
+            });
+
+            user.set("team", object.get("teamcode"));
+            user.save(null, {
+              success: function(temp) {
+                  validTeam();
+                  object.increment("count");
+                  object.save();
+              },
+              error: function(user, error) {
+                invalidTeam("Error: " + error.code + " " + error.message);
+              }
+            })
+        }
+      },
+      error: function(error) {
+        invalidTeam("Error: " + error.code + " " + error.message);
       }
     });
   });
