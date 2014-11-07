efd2f47cb6303c75c8dc10fbd2eb3b203caa9c87
Zachary Liu <liu.zachary@gmail.com>
2014-11-06 22:00:40 -0500

    Resume upload bugfixes

--- js/signup.js
+++ js/signup.js
@@ -1,3 +1,19 @@
+var statuses = {
+    accepted: "Congratulations! We'd like to invite you to attend HackPrinceton! Please fill out and *submit* the confirmation form below by Friday, October 17th.",
+    waitlisted: "Status: Waitlisted - We've received an incredible amount of applications this year (over 2500). We'll be letting people off the waitlist in the coming weeks, so hold on tight!",
+    acceptedWaitlist: "Congratulations! We'd like to invite you to attend HackPrinceton! Please fill out and *submit* the confirmation form below by Friday, October 31st.",
+    pending: "Your status is still pending. Hold on tight while we get decisions out to you!",
+    confirmedGoing: "Thanks for your response! We're excited to see you here!",
+    confirmedNotGoing: "Thanks for your response! We hope to see you at HackPrinceton next Spring!",
+    confirmedAlready: "We've already received your response. If you need to make changes, please email hackprinceton@princetoneclub.com!",
+    resumeUploading: "***PLEASE WAIT*** Resume Uploading"
+};
+
+var errors = {
+    general: "An error occurred. Please email hackprinceton@princetoneclub.com",
+    invalidLogin: "Incorrect Email or Password"
+};
+
 // Google Analytics
 (function (i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r;
@@ -110,7 +126,7 @@ $(document).ready(function () {
         // handle resume data
         var fileUploadControl = $("#fileselect")[0];
         if (fileUploadControl.files.length > 0) {
-            $('#confirm').hide();
+            $('#form').hide();
             $('#loader').show();
             $('#status').text(statuses.resumeUploading);
 
@@ -129,7 +145,7 @@ $(document).ready(function () {
                 user.signUp(null, {
                     success: function (user) {
                         // Hide form, ask to validate email
-                        $('#form').hide();
+                        $('#loader').hide();
                         $('#confirmation').show();
                     },
                     error: function (user, error) {
